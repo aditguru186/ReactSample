8203e77900308938a7e0462d11e38bedf0411790
Aditya Guru <aditguru186@gmail.com>
2022-01-05 00:20:55 +0530

    Alignment readied for the landing page. Disintegrated NavigationBar into a new line.

--- afruno-frontend/src/App.js
+++ afruno-frontend/src/App.js
@@ -2,19 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import axios from "axios";
 import { useEffect, useState } from "react";
-// import NavigationBarCommon from "./navigationBar/NavigationBar";
+import NavigationBarCommon from "./navigationBar/NavigationBar";
+
 // const getFurnitureUrl = "http://192.168.0.102:8080/getFurnitures";
 const getFurnitureUrl = "http://localhost:8080/getFurnitures";
 
@@ -31,72 +25,22 @@ function FurnitureLists() {
   }, []);
   console.log(furnitureList);
   var cardStyle = {
-    display: "inline-block",
-    margin: 20,
-    height: 400,
-    width: 300,
-    padding: 0,
+    // display: "flex",
+    // flexWrap: "nowrap",
+    margin: 10,
+    padding: 20,
     WebkitFilter: "drop-shadow(100px 100px 50px #555)",
     filter: "drop-shadow(5px 5px 50px #555)",
   };
-  return (
-    <div style={cardStyle}>
-      {furnitureList === null ? (
-        <p>Nothing TO show</p>
-      ) : (
-        furnitureList.map((furnitureData, iterator) => (
-          <CardsSetUp
-            furnitureData={furnitureData}
-            key={furnitureData.productId}
-          />
-        ))
-      )}
-    </div>
-  );
-}
-function NavigationBarCommon() {
-  return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Afruno</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
+  var furnitureDump = furnitureList.map((furnitureData) => {
+    if (furnitureList === null) {
+      return <p>Nothing To show</p>;
+    }
+    return (
+      <CardsSetUp furnitureData={furnitureData} key={furnitureData.productId} />
+    );
+  });
+  return <div style={cardStyle}>{furnitureDump}</div>;
 }
 function CardsSetUp(props) {
   var furnitureData = props.furnitureData;
@@ -119,14 +63,6 @@ function CardsSetUp(props) {
   );
 }
 function App() {
-  var furnitureStuff = {
-    id: 1001,
-    furnitureName: "Sofa",
-    imageName:
-      "https://www.ikea.com/in/en/images/products/strandmon-wing-chair-skiftebo-yellow__0325450_pe517970_s5.jpg?f=xl",
-    furnitureDesc: "",
-    price: 10000,
-  };
   return (
     <div>
       <h1>Afruno Welcomes you!</h1>
